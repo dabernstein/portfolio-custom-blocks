30e108f62ba0ce53c514fdeb809f0e1f15352886
Daniel Bernstein <techdbernstein@gmail.com>
2023-11-19 14:59:37 -0500

    Basic About me block
    This is a basic outline for a About me block

---
+++ custom-block.js
@@ -0,0 +1,70 @@
+const {MediaUpload} = wp.blockEditor;
+const {Button} = wp.components;
+
+wp.blocks.registerBlockType('portfolio/custom-block',{
+    title: "About Me Block",
+    icon: 'hammer',
+    category: 'design',
+    attributes: {
+        name: { 
+          type: 'string' 
+        },
+        image: {
+          type: 'string',
+          default: null
+        }
+    },
+    edit: function(props) {
+        function updateName(event) { props.setAttributes({name: event.target.value})}
+
+        function onImageSelect(selectedImage) { props.setAttributes({image: selectedImage.url})}
+
+        return (
+          React.createElement("div", {className: "outer-container"}, 
+            React.createElement("div", {
+              className: "about-container"
+            }, 
+              React.createElement("div", {
+                className: "image-upload-container container"
+              }, 
+                React.createElement("label", null, "Image"), 
+                React.createElement("div", {className: "image-upload"},
+                  props.attributes.image ? React.createElement("img", {
+                    src: props.attributes.image,
+                    alt: "Selected Image",
+                    className: "uploaded-image"
+                  }) : 
+                  React.createElement("div", null, "Select an Image")
+                ),
+                React.createElement(MediaUpload, {
+                  onSelect: onImageSelect,
+                  type: "image",
+                  render: ({open}) => (
+                    React.createElement(Button, {
+                      onClick: open,
+                      className: "button large-button"
+                    },
+                    props.attributes.image ? 'Change Image' : 'Select Image'
+                    )
+                  )
+                })
+              ), 
+              React.createElement("div", {
+                className: "text-content-container container"
+              }, 
+                React.createElement("label", null, "Name: "), 
+                React.createElement("input", {
+                  type: "text",
+                  value: props.attributes.name,
+                  placeholder: "name",
+                  onChange: updateName
+                })
+              )
+            )
+          ));
+    },
+    save: function(props) {
+        return React.createElement("div", null, 
+            React.createElement("h3", null, props.attributes.name));
+    }
+})
---
+++ portfolio-custom-blocks.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Portfolio Custom Blocks
+Description: This plugin provides custom blocks for creating a portfolio
+*/
+
+function portfolio_custom_block_script_register() {
+    wp_enqueue_script(
+        'portfolio-custom-block',
+        plugin_dir_url(__FILE__).'custom-block.js',
+        array('wp-blocks', 'wp-editor', 'wp-components', 'wp-i18n'),
+        true
+    );
+    wp_enqueue_style(
+        'portfolio-editor-styles',
+        plugin_dir_url(__FILE__).'editor-styles.css',
+        array(),
+        '1.0'
+    );
+}
+add_action('enqueue_block_editor_assets', 'portfolio_custom_block_script_register');
+?>
